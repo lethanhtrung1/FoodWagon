6fc15a7e497601a14eea434dde461b6c9db9a791
Le Thanh Trung <trunglt1011@gmail.com>
2024-03-14 13:32:55 +0700

    Add project files.

---
+++ FoodWagon.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FoodWagon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodWagon.DataAccess.Data {
+	public class ApplicationDbContext : DbContext {
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+		//public DbSet<Category> Categories { get; set; }
+
+		//protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		//	base.OnModelCreating(modelBuilder);
+
+		//	modelBuilder.Entity<Category>().HasData(
+		//			new Category { Id = 1, Name = "Appetizer", DisplayOrder = 1 },
+		//			new Category { Id = 2, Name = "Dessert", DisplayOrder = 2 },
+		//			new Category { Id = 3, Name = "Entree", DisplayOrder = 3 }
+		//		);
+		//}
+	}
+}
